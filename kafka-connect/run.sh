89c07123d2202353d6b7f34870d1bdbd6507d36f
adityagarde <aditya.garde96@gmail.com>
2021-05-22 16:38:29 +0530

    Kafka Connect for Twitter

---
+++ kafka-connect/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# run the twitter connector
+connect-standalone connect-standalone.properties twitter.properties
+# OR (linux / mac OSX)
+connect-standalone.sh connect-standalone.properties twitter.properties
